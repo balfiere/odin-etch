75025aa3462213849cbd28d9ade3f18670725e7c
balfiere <balfiere@proton.me>
2024-07-22 16:12:36 -0500

    add button to reset grid size based on prompt

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-container = document.querySelector("#container");
-
-let gridSize = 16;
-for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-        let div = document.createElement("div");
-        div.className = "grid";
-        container.appendChild(div);
+function makeGrid(gridSize) {
+    container.innerHTML = "";
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            let div = document.createElement("div");
+            div.className = "grid";
+            container.appendChild(div);
+        }
     }
+
+    let containerSize = getComputedStyle(document.body).getPropertyValue("--containerSize");
+    let containerGap = getComputedStyle(document.body).getPropertyValue("--gap");
+    
+    let divSize = (parseInt(containerSize) - (parseInt(containerGap) * (gridSize) ) ) / gridSize;
+
+    document.querySelectorAll(".grid").forEach(div => {
+        div.style.width = divSize + "px";
+        div.style.height = divSize + "px";
+    });
 }
 
 function changeColor(event) {
-    event.target.classList.add("hover");
+    if (event.target.id != "container") {
+        event.target.classList.add("hover");
+    }
 }
 
-container.addEventListener("mouseover", changeColor);
+function reset() {
+    let gridSize = prompt("Please enter new grid size (1-100)");
+    gridSize = parseInt(gridSize);
+    while (gridSize < 1 || gridSize > 100 || isNaN(gridSize)) {
+        gridSize = prompt("Invalid input, please enter new grid size (1-100)");
+    }
+
+    makeGrid(gridSize);
+}
+
+let container = document.querySelector("#container");
+
+let gridSize = 16;
+makeGrid(gridSize);
+
+container.addEventListener("mouseover", changeColor, false);
+
+let resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", reset, false);
