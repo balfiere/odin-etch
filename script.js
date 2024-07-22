7fdb772396863de251e9f67b409a3b9fa650597a
balfiere <balfiere@proton.me>
2024-07-22 15:17:53 -0500

    create grid

---
+++ script.js
@@ -0,0 +1,11 @@
+container = document.querySelector("#container");
+
+let gridSize = 16;
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+        let div = document.createElement("div");
+        div.className = "grid";
+        container.appendChild(div);
+    }
+}
+
